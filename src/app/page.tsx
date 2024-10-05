a88b5e019e3afa7f5d3689a124b1efec58656d1b
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2024-10-05 11:18:24 +0900

    add: fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,19 @@ import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
 import { IoMdMail } from "react-icons/io";
 
+import clsx from "clsx";
+import { Cormorant, Roboto } from "next/font/google";
+
+const cormorant = Cormorant({
+  subsets: ["latin"],
+  weight: "600",
+});
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["300", "400"],
+});
+
 export default function Home() {
   return (
     <>
@@ -10,43 +23,53 @@ export default function Home() {
         <nav>
           <ul>
             <li>
-              <Link href={"/"}>Home</Link>
+              <Link href={"/"} className={clsx(roboto.className)}>
+                Home
+              </Link>
             </li>
             <li>
-              <Link href={"/#about"}>About</Link>
+              <Link href={"/#about"} className={clsx(roboto.className)}>
+                About
+              </Link>
             </li>
             <li>
-              <Link href={"/#works"}>Works</Link>
+              <Link href={"/#works"} className={clsx(roboto.className)}>
+                Works
+              </Link>
             </li>
             <li>
-              <Link href={"/#contact"}>Contact</Link>
+              <Link href={"/#contact"} className={clsx(roboto.className)}>
+                Contact
+              </Link>
             </li>
           </ul>
         </nav>
       </header>
       <main>
-        <h1>Tatsuya Hasegawa</h1>
+        <h1 className={clsx(cormorant.className)}>Tatsuya Hasegawa</h1>
         <section id="about">
-          <h2>About</h2>
+          <h2 className={clsx(roboto.className)}>About</h2>
           <p>
             フロントエンドエンジニア 長谷川 達也のポートフォリオサイトです。
             実際に触れることのできるフロントエンドを開発することに喜びを感じます。
             書いたコードが画面に反映される感動を学習を始めて以来ずっと持ち続けています。
           </p>
           <section>
-            <h3>Tatsuya Hasegawa</h3>
-            <p>Front-end Developer</p>
+            <h3 className={clsx(roboto.className)}>Tatsuya Hasegawa</h3>
+            <p className={clsx(roboto.className)}>Front-end Developer</p>
             <Link href={"https://github.com/thasegawakaihatsu"} target="_brank">
               <FaGithub />
             </Link>
             <Link href={"mailto:hasegawatatsuya206@gmail.com"} target="_brank">
               <IoMdMail />
             </Link>
-            <Link href={"/"}>More</Link>
+            <Link href={"/"} className={clsx(roboto.className)}>
+              More
+            </Link>
           </section>
         </section>
         <section id="works">
-          <h2>Works</h2>
+          <h2 className={clsx(roboto.className)}>Works</h2>
           <section>
             <h3>一問一答メーカー</h3>
             <p>Webアプリ</p>
@@ -68,20 +91,24 @@ export default function Home() {
             />
           </section>
           <section>
-            <h3>under development...</h3>
+            <h3 className={clsx(roboto.className)}>under development...</h3>
           </section>
         </section>
         <section id="contact">
-          <h2>Contact</h2>
+          <h2 className={clsx(roboto.className)}>Contact</h2>
           <Link href={"mailto:hasegawatatsuya206@gmail.com"}>
             <IoMdMail />
-            <span>hasegawatatsuya206@gmail.com</span>
+            <span className={clsx(roboto.className)}>
+              hasegawatatsuya206@gmail.com
+            </span>
           </Link>
         </section>
       </main>
       <footer>
         <p>
-          <small>© 2024 Tatsuya Hasegawa</small>
+          <small className={clsx(roboto.className)}>
+            © 2024 Tatsuya Hasegawa
+          </small>
         </p>
       </footer>
     </>
