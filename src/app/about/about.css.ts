e19df3f24e76a7d7900e3dbc4188830c5df0c8eb
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2024-10-11 15:18:47 +0900

    add: style desktop size

--- src/app/about/about.css.ts
+++ src/app/about/about.css.ts
@@ -1,8 +1,13 @@
+import { breakpoints } from "@/styles/styles.css";
 import { keyframes, style } from "@vanilla-extract/css";
+
+export const glassBox = style({
+  position: "relative",
+});
 
 export const aboutHero = style({
   position: "relative",
-  margin: "120px auto 24px",
+  margin: "120px auto 40px",
   height: 480,
 });
 export const nameBox = style({
@@ -48,4 +53,92 @@ export const photo = style({
   animationDuration: "8s",
   animationTimingFunction: "linear",
   animationIterationCount: "infinite",
+});
+
+export const profileLinkBox = style({
+  display: "flex",
+  gap: 24,
+  justifyContent: "center",
+  marginBottom: 80,
+});
+export const profileLinkIcon = style({
+  width: 28,
+  height: 28,
+  color: "#fff",
+  transition: "scale 0.3s ease",
+  ":hover": {
+    scale: 1.3,
+  },
+});
+
+export const animateLine = keyframes({
+  "0%": { transform: "translateY(-100%)" },
+  "100%": { transform: "translateY(100%)" },
+});
+export const line = style({
+  position: "relative",
+  width: 1,
+  height: 60,
+  margin: "0 auto 120px",
+  backgroundColor: "rgba(19, 0, 89, .3)",
+  overflow: "hidden",
+  ":before": {
+    content: "",
+    position: "absolute",
+    width: 1,
+    height: 60,
+    backgroundColor: "#fff",
+    animationName: animateLine,
+    animationDuration: "1.6s",
+    animationTimingFunction: "ease",
+    animationIterationCount: "infinite",
+  },
+});
+
+export const occupation = style({
+  fontSize: "6.8rem",
+  fontWeight: 300,
+  color: "#fff",
+  textAlign: "center",
+  marginBottom: 160,
+});
+
+export const description = style({
+  fontSize: "2.4rem",
+  fontWeight: 100,
+  color: "#fff",
+  letterSpacing: 1,
+  lineHeight: 1.6,
+  textAlign: "center",
+  marginBottom: 240,
+});
+
+export const backToIndex = style({
+  position: "absolute",
+  bottom: 100,
+  left: "clamp(40px, 15.2%, 300px)",
+  fontSize: "2.0rem",
+  fontWeight: 300,
+  color: "#fff",
+  textDecoration: "none",
+  selectors: {
+    "&::before": {
+      content: "",
+      position: "absolute",
+      top: 28,
+      display: "block",
+      width: "100%",
+      height: 12,
+      backgroundColor: "#fff",
+      transition: "height 0.3s ease",
+    },
+    "&:hover::before": {
+      height: 1,
+    },
+  },
+  "@media": {
+    [breakpoints["2xl"]]: {
+      left: "clamp(40px, 10%, 240px)",
+    },
+  },
 });
--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -2,11 +2,18 @@
 
 import {
   aboutHero,
+  backToIndex,
+  description,
+  glassBox,
+  line,
   nameBox,
   nameEn,
   nameJa,
+  occupation,
   photo,
   photoBox,
+  profileLinkBox,
+  profileLinkIcon,
 } from "@/app/about/about.css";
 import { isOpenHamburgerMenuAtom } from "@/atoms/isOpenHamburgerMenuAtom";
 import AudioButton from "@/components/audio/audioButton";
@@ -15,11 +22,14 @@ import Glass from "@/components/glass/glass";
 import HamburgerMenu from "@/components/hamburgerMenu/hamburgerMenu";
 import MobileNav from "@/components/mobileNav/mobileNav";
 import Nav from "@/components/nav/nav";
+import Object1 from "@/components/object/object1";
+import Object2 from "@/components/object/object2";
+import Object3 from "@/components/object/object3";
 import TextCircle from "@/components/textCircle/textCircle";
 import { header } from "@/styles/styles.css";
 import clsx from "clsx";
 import { useAtomValue } from "jotai";
-import { Cormorant, Noto_Serif_JP } from "next/font/google";
+import { Cormorant, Noto_Serif_JP, Roboto } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
 import { FaGithub } from "react-icons/fa";
@@ -31,69 +41,92 @@ const notoSerifJP = Noto_Serif_JP({
   weight: "400",
 });
 
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["300", "400"],
+});
+
 const cormorant = Cormorant({
   subsets: ["latin"],
-  weight: "400",
+  weight: ["300", "400"],
 });
 
 export default function About() {
   const isOpenHamburgerMenu = useAtomValue(isOpenHamburgerMenuAtom);
 
   return (
-    <Glass>
-      <header className={clsx(header)}>
-        <Nav />
-        <HamburgerMenu />
-        {isOpenHamburgerMenu && <MobileNav />}
-        <AudioButton />
-        <TextCircle />
-      </header>
-      <main>
-        <div className={clsx(aboutHero)}>
-          <h1 className={nameBox}>
-            <span className={clsx(notoSerifJP.className, nameJa)}>
-              ハセガワ タツヤ
-            </span>
-            <span className={clsx(cormorant.className, nameEn)}>
-              Tatsuya Hasegawa
-            </span>
-          </h1>
-          <Tilt reset={false}>
-            <div className={clsx(photoBox)}>
-              <Image
-                src={"/photo.webp"}
-                width={320}
-                height={480}
-                alt="長谷川達也"
-                className={photo}
-                view-transition-name={"photo"}
-              />
+    <>
+      <div className={clsx(glassBox)}>
+        <Glass>
+          <header className={clsx(header)}>
+            <Nav />
+            <HamburgerMenu />
+            {isOpenHamburgerMenu && <MobileNav />}
+            <AudioButton />
+            <TextCircle />
+          </header>
+          <main>
+            <div className={clsx(aboutHero)}>
+              <h1 className={nameBox}>
+                <span className={clsx(notoSerifJP.className, nameJa)}>
+                  ハセガワ タツヤ
+                </span>
+                <span className={clsx(cormorant.className, nameEn)}>
+                  Tatsuya Hasegawa
+                </span>
+              </h1>
+              <Tilt reset={false}>
+                <div className={clsx(photoBox)}>
+                  <Image
+                    src={"/photo.webp"}
+                    width={320}
+                    height={480}
+                    alt="長谷川達也"
+                    className={photo}
+                    view-transition-name={"photo"}
+                  />
+                </div>
+              </Tilt>
             </div>
-          </Tilt>
-        </div>
-        <Link href={"https://github.com/thasegawakaihatsu"} target="_brank">
-          <FaGithub />
+            <div className={clsx(profileLinkBox)}>
+              <Link
+                href={"https://github.com/thasegawakaihatsu"}
+                target="_brank"
+              >
+                <FaGithub className={clsx(profileLinkIcon)} />
+              </Link>
+              <Link
+                href={"mailto:hasegawatatsuya206@gmail.com"}
+                target="_brank"
+              >
+                <IoMdMail className={clsx(profileLinkIcon)} />
+              </Link>
+            </div>
+            <div className={clsx(line)}></div>
+            <p className={clsx(cormorant.className, occupation)}>
+              Web Design,
+              <br />
+              Frontend Development
+            </p>
+            <p className={clsx(description)}>
+              2001年生まれ、ECCコンピュータ専門学校中退。
+              <br />
+              趣味は散歩とランニング。
+              <br />
+              有酸素運動で汗を流すのが日課。
+              <br />
+              Webの豊かな表現を身につけるために 日々学習に取り組んでいます。
+            </p>
+          </main>
+          <Footer />
+        </Glass>
+        <Link href={"/"} className={(roboto.className, backToIndex)}>
+          Back to Index
         </Link>
-        <Link href={"mailto:hasegawatatsuya206@gmail.com"} target="_brank">
-          <IoMdMail />
-        </Link>
-        <p>
-          Web Design,
-          <br />
-          Frontend Development
-        </p>
-        <p>
-          2001年生まれ、ECCコンピュータ専門学校中退。
-          <br />
-          趣味は散歩とランニング。
-          <br />
-          Webの豊かな表現を身につけるために
-          <br />
-          日々学習に取り組んでいます。
-        </p>
-        <Link href={"/"}>Back to Index</Link>
-      </main>
-      <Footer />
-    </Glass>
+      </div>
+      <Object1 />
+      <Object2 />
+      <Object3 />
+    </>
   );
 }
