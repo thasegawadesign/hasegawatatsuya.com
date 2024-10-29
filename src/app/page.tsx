ee0e371c383f43beca8dc1b6dc4360e6aad2acee
thasegawadesign <hasegawatatsuya206@gmail.com>
2024-10-29 14:45:28 +0900

    fix: description web works

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -253,7 +253,7 @@ export default function Home() {
               <br className={clsx(desktopBr)} />
               実際に触れることのできる
               <br className={mobileBr} />
-              UIをデザイン・実装することに
+              Web作品をデザイン・実装することに
               <br className={mobileBr} />
               喜びを感じます。
               <br className={mobileBr} />
