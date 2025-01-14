c790ca5ff06e0577fd94f8738f166b04418aca26
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-01-14 17:49:53 +0900

    feat: desktop style

--- src/app/works/fleur/page.css.ts
+++ src/app/works/fleur/page.css.ts
@@ -20,3 +20,35 @@ export const fleurHeadingJa = style({
   color: vars.color.text,
   marginLeft: "3.6vw",
 });
+
+export const worksDl = style({
+  display: "flex",
+  flexDirection: "column",
+  gap: "5.2vw",
+  marginTop: "7.6vw",
+  marginBottom: "7.2vw",
+});
+export const worksDlItem = style({
+  display: "flex",
+  justifyContent: "center",
+  gap: "2.4vw",
+});
+export const worksDt = style({
+  fontSize: "1.2rem",
+  fontWeight: 300,
+  color: vars.color.text,
+  width: 48,
+  textAlign: "right",
+});
+export const worksDd = style({
+  fontSize: "clamp(1.6rem, 1.4vw, 2.4rem)",
+  fontWeight: 300,
+  color: vars.color.text,
+  width: "48vw",
+});
+export const worksRole = style({
+  fontSize: "clamp(2.8rem, 3vw, 4.8rem)",
+  color: vars.color.text,
+  lineHeight: "1.28em",
+  width: "48vw",
+});
--- src/app/works/fleur/page.tsx
+++ src/app/works/fleur/page.tsx
@@ -1,6 +1,17 @@
 "use client";
 
 import { header, main } from "@/app/page.css";
+import {
+  fleurHeadingEn,
+  fleurHeadingHgroup,
+  fleurHeadingJa,
+  fleurImage,
+  worksDd,
+  worksDl,
+  worksDlItem,
+  worksDt,
+  worksRole,
+} from "@/app/works/fleur/page.css";
 import { isOpenHamburgerMenuAtom } from "@/atoms/isOpenHamburgerMenuAtom";
 import AudioPlayer from "@/components/audio/audioPlayer";
 import Footer from "@/components/footer/footer";
@@ -14,19 +25,18 @@ import Object3 from "@/components/object/object3";
 import TextCircle from "@/components/textCircle/textCircle";
 import clsx from "clsx";
 import { useAtomValue } from "jotai";
-import { Cormorant } from "next/font/google";
+import { Cormorant, Roboto } from "next/font/google";
 import Image from "next/image";
 import Link from "next/link";
-import {
-  fleurHeadingEn,
-  fleurHeadingHgroup,
-  fleurHeadingJa,
-  fleurImage,
-} from "./page.css";
 
 const cormorant = Cormorant({
   subsets: ["latin"],
   weight: ["500", "600"],
+});
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["300", "400"],
 });
 
 export default function Fleur() {
@@ -58,43 +68,58 @@ export default function Fleur() {
               フルール
             </p>
           </hgroup>
-          <dl>
-            <dt>What :</dt>
-            <dd>
-              見た目が美しいスイーツが好きで、健康志向な人のためのマカロン専門店。
-            </dd>
-            <dt>Why :</dt>
-            <dd>
-              トレンドに敏感な特定のターゲット層を想定したWebサイト制作を経験してみたいと思い、制作しました。
-              <br />
-              口にするものには特別感を求める健康志向な人に届けるために、グルテンフリー・美しいビジュアル・清潔感をキーワードにしました。
-            </dd>
-            <dt>How :</dt>
-            <dd>
-              職人の技が輝く仕上げの美しいマカロンを際立たせるためにシンプルで清潔感のある白基調のデザインに仕上げました。
-              <br />
-              「フルール」はフランス語で花という意味で、花のように小さくてかわいいマカロンを看板商品としていることから意味を拝借してブランド名を決めました。
-              <br />
-              ビジュアルデザインにおいてはモチーフを使用できないかと考え、ブランド名の由来である花の装飾を施しました。また、マカロンの丸みをセクションの背景の造形に活用しました。
-              <br />
-              実装面では、操作可能な要素には控えめなインタラクションを設計して細部を作り込みました。
-            </dd>
-            <dt>Role :</dt>
-            <dd>
-              Web Design,
-              <br />
-              Frontend Development
-            </dd>
-            <dt>Time :</dt>
-            <dd>
-              制作にかかった時間はデザイン14時間、コーディング14時間、合計で28時間でした。
-            </dd>
-            <dt>URL :</dt>
-            <dd>
-              <Link href={"https://fleur-website.vercel.app/"} target="_blank">
-                https://fleur-website.vercel.app/
-              </Link>
-            </dd>
+          <dl className={clsx(worksDl)}>
+            <div className={clsx(worksDlItem)}>
+              <dt className={clsx(roboto.className, worksDt)}>What :</dt>
+              <dd className={clsx(worksDd)}>
+                見た目が美しいスイーツが好きで、健康志向な人のためのマカロン専門店。
+              </dd>
+            </div>
+            <div className={clsx(worksDlItem)}>
+              <dt className={clsx(roboto.className, worksDt)}>Why :</dt>
+              <dd className={clsx(worksDd)}>
+                トレンドに敏感な特定のターゲット層を想定したWebサイト制作を経験してみたいと思い、制作しました。
+                <br />
+                口にするものには特別感を求める健康志向な人に届けるために、グルテンフリー・美しいビジュアル・清潔感をキーワードにしました。
+              </dd>
+            </div>
+            <div className={clsx(worksDlItem)}>
+              <dt className={clsx(roboto.className, worksDt)}>How :</dt>
+              <dd className={clsx(worksDd)}>
+                職人の技が輝く仕上げの美しいマカロンを際立たせるためにシンプルで清潔感のある白基調のデザインに仕上げました。
+                <br />
+                「フルール」はフランス語で花という意味で、花のように小さくてかわいいマカロンを看板商品としていることから意味を拝借してブランド名を決めました。
+                <br />
+                ビジュアルデザインにおいてはモチーフを使用できないかと考え、ブランド名の由来である花の装飾を施しました。また、マカロンの丸みをセクションの背景の造形に活用しました。
+                <br />
+                実装面では、操作可能な要素には控えめなインタラクションを設計して細部を作り込みました。
+              </dd>
+            </div>
+            <div className={clsx(worksDlItem)}>
+              <dt className={clsx(roboto.className, worksDt)}>Role :</dt>
+              <dd className={clsx(cormorant.className, worksRole)}>
+                Web Design,
+                <br />
+                Frontend Development
+              </dd>
+            </div>
+            <div className={clsx(worksDlItem)}>
+              <dt className={clsx(roboto.className, worksDt)}>Time :</dt>
+              <dd className={clsx(worksDd)}>
+                制作にかかった時間はデザイン14時間、コーディング14時間、合計で28時間でした。
+              </dd>
+            </div>
+            <div className={clsx(worksDlItem)}>
+              <dt className={clsx(roboto.className, worksDt)}>URL :</dt>
+              <dd className={clsx(worksDd)}>
+                <Link
+                  href={"https://fleur-website.vercel.app/"}
+                  target="_blank"
+                >
+                  https://fleur-website.vercel.app/
+                </Link>
+              </dd>
+            </div>
           </dl>
         </main>
         <Footer />
