a5b1d33c0483627688dec410dcf73a092c442666
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-01-17 20:28:32 +0900

    refactor: divide worksInfo components

--- src/app/works/fleur/page.tsx
+++ src/app/works/fleur/page.tsx
@@ -2,18 +2,6 @@
 
 import { glassBox } from "@/app/about/page.css";
 import { header, main } from "@/app/page.css";
-import {
-  fleurHeadingEn,
-  fleurHeadingHgroup,
-  fleurHeadingJa,
-  fleurImage,
-  worksDd,
-  worksDl,
-  worksDlItem,
-  worksDt,
-  worksLink,
-  worksRole,
-} from "@/app/works/fleur/page.css";
 import { isOpenHamburgerMenuAtom } from "@/atoms/isOpenHamburgerMenuAtom";
 import AudioPlayer from "@/components/audio/audioPlayer";
 import BackToIndex from "@/components/backToIndex/backToIndex";
@@ -26,51 +14,15 @@ import Object1 from "@/components/object/object1";
 import Object2 from "@/components/object/object2";
 import Object3 from "@/components/object/object3";
 import TextCircle from "@/components/textCircle/textCircle";
+import WorksInfo from "@/components/works/worksInfo";
 import useSmoothScroll from "@/hooks/useSmoothScroll";
-import { gsapAnimation } from "@/utils/gsap";
 import clsx from "clsx";
-import gsap from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { useAtomValue } from "jotai";
-import { Cormorant, Roboto } from "next/font/google";
-import Image from "next/image";
-import Link from "next/link";
-import { useEffect, useRef } from "react";
-
-const cormorant = Cormorant({
-  subsets: ["latin"],
-  weight: ["500", "600"],
-});
-
-const roboto = Roboto({
-  subsets: ["latin"],
-  weight: ["300", "400"],
-});
 
 export default function Fleur() {
-  const hgroupRef = useRef(null);
-  const worksWhatRef = useRef(null);
-  const worksWhyRef = useRef(null);
-  const worksHowRef = useRef(null);
-  const worksRoleRef = useRef(null);
-  const worksTimeRef = useRef(null);
-  const worksURLRef = useRef(null);
-
   const isOpenHamburgerMenu = useAtomValue(isOpenHamburgerMenuAtom);
 
   useSmoothScroll();
-
-  useEffect(() => {
-    gsap.registerPlugin(ScrollTrigger);
-
-    gsapAnimation.inview(hgroupRef);
-    gsapAnimation.inview(worksWhatRef);
-    gsapAnimation.inview(worksWhyRef);
-    gsapAnimation.inview(worksHowRef);
-    gsapAnimation.inview(worksRoleRef);
-    gsapAnimation.inview(worksTimeRef);
-    gsapAnimation.inview(worksURLRef);
-  }, []);
 
   return (
     <>
@@ -84,42 +36,29 @@ export default function Fleur() {
             <TextCircle />
           </header>
           <main className={clsx(main)}>
-            <Image
-              src={"/fleur-website-screen-shot.webp"}
-              width={2640}
-              height={1520}
-              alt="架空マカロン専門店フルール"
-              className={clsx(fleurImage)}
-              priority
-            />
-            <hgroup ref={hgroupRef} className={clsx(fleurHeadingHgroup)}>
-              <h1 className={clsx(cormorant.className, fleurHeadingEn)}>
-                Fleur
-              </h1>
-              <p className={clsx(fleurHeadingJa)}>
-                架空マカロン専門店
-                <br />
-                フルール
-              </p>
-            </hgroup>
-            <dl className={clsx(worksDl)}>
-              <div ref={worksWhatRef} className={clsx(worksDlItem)}>
-                <dt className={clsx(roboto.className, worksDt)}>What :</dt>
-                <dd className={clsx(worksDd)}>
-                  見た目が美しいスイーツが好きで、健康志向な人のための架空マカロン専門店フルールのブランドサイト。
-                </dd>
-              </div>
-              <div ref={worksWhyRef} className={clsx(worksDlItem)}>
-                <dt className={clsx(roboto.className, worksDt)}>Why :</dt>
-                <dd className={clsx(worksDd)}>
+            <WorksInfo
+              nameJa="架空マカロン専門店フルール"
+              nameEn="Fleur"
+              nameJaNode={
+                <>
+                  架空マカロン専門店
+                  <br />
+                  フルール
+                </>
+              }
+              imageSrc="/fleur-website-screen-shot.webp"
+              imageWidth={2640}
+              imageHeight={1520}
+              what="見た目が美しいスイーツが好きで、健康志向な人のための架空マカロン専門店フルールのブランドサイト。"
+              why={
+                <>
                   トレンドに敏感な特定のターゲット層を想定したWebサイト制作を経験してみたいと思い、制作しました。
                   <br />
                   口にするものには特別感を求める健康志向な人に届けるために、グルテンフリー・美しいビジュアル・清潔感をキーワードにしました。
-                </dd>
-              </div>
-              <div ref={worksHowRef} className={clsx(worksDlItem)}>
-                <dt className={clsx(roboto.className, worksDt)}>How :</dt>
-                <dd className={clsx(worksDd)}>
+                </>
+              }
+              how={
+                <>
                   職人の技が輝く仕上げの美しいマカロンを際立たせるためにシンプルで清潔感のある白基調のデザインに仕上げました。
                   <br />
                   「フルール」はフランス語で花という意味で、花のように小さくてかわいいマカロンを看板商品としていることから意味を拝借してブランド名を決めました。
@@ -127,35 +66,18 @@ export default function Fleur() {
                   ビジュアルデザインにおいてはモチーフを使用できないかと考え、ブランド名の由来である花の装飾を施しました。また、マカロンの丸みをセクションの背景の造形に活用しました。
                   <br />
                   実装面では、操作可能な要素には控えめなインタラクションを設計して細部を作り込みました。
-                </dd>
-              </div>
-              <div ref={worksRoleRef} className={clsx(worksDlItem)}>
-                <dt className={clsx(roboto.className, worksDt)}>Role :</dt>
-                <dd className={clsx(cormorant.className, worksRole)}>
+                </>
+              }
+              role={
+                <>
                   Web Design,
                   <br />
                   Frontend Development
-                </dd>
-              </div>
-              <div ref={worksTimeRef} className={clsx(worksDlItem)}>
-                <dt className={clsx(roboto.className, worksDt)}>Time :</dt>
-                <dd className={clsx(worksDd)}>
-                  制作にかかった時間はデザイン14時間、コーディング14時間、合計で28時間でした。
-                </dd>
-              </div>
-              <div ref={worksURLRef} className={clsx(worksDlItem)}>
-                <dt className={clsx(roboto.className, worksDt)}>URL :</dt>
-                <dd className={clsx(worksDd)}>
-                  <Link
-                    href={"https://fleur-website.vercel.app/"}
-                    target="_blank"
-                    className={clsx(worksLink)}
-                  >
-                    https://fleur-website.vercel.app/
-                  </Link>
-                </dd>
-              </div>
-            </dl>
+                </>
+              }
+              time="制作にかかった時間はデザイン14時間、コーディング14時間、合計で28時間でした。"
+              URL="https://fleur-website.vercel.app/"
+            />
           </main>
           <Footer />
         </Glass>
---
+++ src/components/works/worksInfo.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import {
+  hgroupHeading,
+  hgroupHeadingEn,
+  hgroupHeadingJa,
+  worksDd,
+  worksDl,
+  worksDlItem,
+  worksDt,
+  worksImage,
+  worksLink,
+  worksRole,
+} from "@/components/works/worksInfo.css";
+import { gsapAnimation } from "@/utils/gsap";
+import clsx from "clsx";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { Cormorant, Roboto } from "next/font/google";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useEffect, useRef } from "react";
+
+const cormorant = Cormorant({
+  subsets: ["latin"],
+  weight: ["500", "600"],
+});
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["300", "400"],
+});
+
+type Props = {
+  nameJa: string;
+  nameEn: string;
+  nameJaNode?: React.ReactNode;
+  nameEnNode?: React.ReactNode;
+  imageSrc: string;
+  imageWidth: number;
+  imageHeight: number;
+  what: string | React.ReactNode;
+  why: string | React.ReactNode;
+  how: string | React.ReactNode;
+  role: string | React.ReactNode;
+  time: string | React.ReactNode;
+  URL?: string;
+};
+
+export default function WorksInfo(props: Props) {
+  const {
+    nameJa,
+    nameEn,
+    nameJaNode,
+    imageSrc,
+    imageWidth,
+    imageHeight,
+    what,
+    why,
+    how,
+    role,
+    time,
+    URL,
+  } = props;
+
+  const hgroupRef = useRef(null);
+  const worksWhatRef = useRef(null);
+  const worksWhyRef = useRef(null);
+  const worksHowRef = useRef(null);
+  const worksRoleRef = useRef(null);
+  const worksTimeRef = useRef(null);
+  const worksURLRef = useRef(null);
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsapAnimation.inview(hgroupRef);
+    gsapAnimation.inview(worksWhatRef);
+    gsapAnimation.inview(worksWhyRef);
+    gsapAnimation.inview(worksHowRef);
+    gsapAnimation.inview(worksRoleRef);
+    gsapAnimation.inview(worksTimeRef);
+    gsapAnimation.inview(worksURLRef);
+  }, []);
+
+  return (
+    <>
+      <Image
+        src={imageSrc}
+        width={imageWidth}
+        height={imageHeight}
+        alt={nameJa}
+        className={clsx(worksImage)}
+        priority
+      />
+      <hgroup ref={hgroupRef} className={clsx(hgroupHeading)}>
+        <h1 className={clsx(cormorant.className, hgroupHeadingEn)}>{nameEn}</h1>
+        <p className={clsx(hgroupHeadingJa)}>{nameJaNode}</p>
+      </hgroup>
+      <dl className={clsx(worksDl)}>
+        <div ref={worksWhatRef} className={clsx(worksDlItem)}>
+          <dt className={clsx(roboto.className, worksDt)}>What :</dt>
+          <dd className={clsx(worksDd)}>{what}</dd>
+        </div>
+        <div ref={worksWhyRef} className={clsx(worksDlItem)}>
+          <dt className={clsx(roboto.className, worksDt)}>Why :</dt>
+          <dd className={clsx(worksDd)}>{why}</dd>
+        </div>
+        <div ref={worksHowRef} className={clsx(worksDlItem)}>
+          <dt className={clsx(roboto.className, worksDt)}>How :</dt>
+          <dd className={clsx(worksDd)}>{how}</dd>
+        </div>
+        <div ref={worksRoleRef} className={clsx(worksDlItem)}>
+          <dt className={clsx(roboto.className, worksDt)}>Role :</dt>
+          <dd className={clsx(cormorant.className, worksRole)}>{role}</dd>
+        </div>
+        <div ref={worksTimeRef} className={clsx(worksDlItem)}>
+          <dt className={clsx(roboto.className, worksDt)}>Time :</dt>
+          <dd className={clsx(worksDd)}>{time}</dd>
+        </div>
+        <div ref={worksURLRef} className={clsx(worksDlItem)}>
+          <dt className={clsx(roboto.className, worksDt)}>URL :</dt>
+          <dd className={clsx(worksDd)}>
+            {URL && (
+              <Link href={URL} target="_blank" className={clsx(worksLink)}>
+                {URL}
+              </Link>
+            )}
+          </dd>
+        </div>
+      </dl>
+    </>
+  );
+}
