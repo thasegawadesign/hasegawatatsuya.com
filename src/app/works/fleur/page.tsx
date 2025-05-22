a25c0b1f8a9f0e6b4f164863848f36ecb2c391bb
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-05-22 22:27:35 +0900

    fix: metadata title

--- src/app/works/fleur/page.tsx
+++ src/app/works/fleur/page.tsx
@@ -1,91 +1,15 @@
-"use client";
+import Works from "@/app/works/fleur/works";
+import { Metadata } from "next";
 
-import { glassBox } from "@/app/about/page.css";
-import { header, main } from "@/app/page.css";
-import { isOpenHamburgerMenuAtom } from "@/atoms/isOpenHamburgerMenuAtom";
-import AudioPlayer from "@/components/audio/audioPlayer";
-import BackToIndex from "@/components/backToIndex/backToIndex";
-import Footer from "@/components/footer/footer";
-import Glass from "@/components/glass/glass";
-import HamburgerMenu from "@/components/hamburgerMenu/hamburgerMenu";
-import MobileNav from "@/components/mobileNav/mobileNav";
-import Nav from "@/components/nav/nav";
-import Object1 from "@/components/object/object1";
-import Object2 from "@/components/object/object2";
-import Object3 from "@/components/object/object3";
-import TextCircle from "@/components/textCircle/textCircle";
-import WorksInfo from "@/components/works/worksInfo";
-import useSmoothScroll from "@/hooks/useSmoothScroll";
-import clsx from "clsx";
-import { useAtomValue } from "jotai";
+export const metadata: Metadata = {
+  title: "架空マカロン専門店フルール | Tatsuya Hasegawa | Webデザイナー",
+  description: "Tatsuya Hasegawa のポートフォリオサイト",
+};
 
 export default function Fleur() {
-  const isOpenHamburgerMenu = useAtomValue(isOpenHamburgerMenuAtom);
-
-  useSmoothScroll();
-
   return (
     <>
-      <div className={clsx(glassBox)}>
-        <Glass>
-          <header className={clsx(header)}>
-            <Nav />
-            <HamburgerMenu />
-            {isOpenHamburgerMenu && <MobileNav />}
-            <AudioPlayer />
-            <TextCircle />
-          </header>
-          <main className={clsx(main)}>
-            <WorksInfo
-              nameJa="架空マカロン専門店フルール"
-              nameEn="Fleur"
-              nameJaNode={
-                <>
-                  架空マカロン専門店
-                  <br />
-                  フルール
-                </>
-              }
-              imageSrc="/fleur-website-screen-shot.webp"
-              imageWidth={2640}
-              imageHeight={1520}
-              what="健康志向でビジュアルが美しいスイーツが好きな人のための架空マカロン専門店フルールのブランドサイト"
-              why={
-                <>
-                  トレンドに敏感なターゲット層を想定したWebサイト制作を経験してみたいと思い、制作しました。
-                  <br />
-                  口にするものには特別感を求める健康志向な人に届けるために、グルテンフリー・美・清潔感をデザインのキーワードにしました。
-                </>
-              }
-              how={
-                <>
-                  職人の技が輝く仕上げの美しいマカロンを際立たせるために、シンプルで清潔感のある白基調のデザインに仕上げました。
-                  <br />
-                  「フルール」はフランス語で花という意味で、花のように小さくてかわいいマカロンを看板商品としていることから命名しました。
-                  <br />
-                  ビジュアルデザインにおいてはモチーフを使用できないかと考え、ブランド名の由来である花の装飾を施しました。また、マカロンの丸みをセクションの造形に活用しました。
-                  <br />
-                  技術面では、操作可能な要素には控えめなインタラクションを実装して細部を作り込みました。
-                </>
-              }
-              role={
-                <>
-                  Web Design,
-                  <br />
-                  Frontend Development
-                </>
-              }
-              date="2024年11月"
-              URL="https://www.fleur.vegetworks.com/"
-            />
-          </main>
-          <Footer />
-        </Glass>
-        <BackToIndex />
-      </div>
-      <Object1 />
-      <Object2 />
-      <Object3 />
+      <Works />
     </>
   );
 }
--- src/app/works/playlist2025/page.tsx
+++ src/app/works/playlist2025/page.tsx
@@ -1,79 +1,15 @@
-"use client";
+import Works from "@/app/works/playlist2025/works";
+import { Metadata } from "next";
 
-import { glassBox } from "@/app/about/page.css";
-import { header, main } from "@/app/page.css";
-import { isOpenHamburgerMenuAtom } from "@/atoms/isOpenHamburgerMenuAtom";
-import AudioPlayer from "@/components/audio/audioPlayer";
-import BackToIndex from "@/components/backToIndex/backToIndex";
-import Footer from "@/components/footer/footer";
-import Glass from "@/components/glass/glass";
-import HamburgerMenu from "@/components/hamburgerMenu/hamburgerMenu";
-import MobileNav from "@/components/mobileNav/mobileNav";
-import Nav from "@/components/nav/nav";
-import Object1 from "@/components/object/object1";
-import Object2 from "@/components/object/object2";
-import Object3 from "@/components/object/object3";
-import TextCircle from "@/components/textCircle/textCircle";
-import WorksInfo from "@/components/works/worksInfo";
-import useSmoothScroll from "@/hooks/useSmoothScroll";
-import clsx from "clsx";
-import { useAtomValue } from "jotai";
+export const metadata: Metadata = {
+  title: "プレイリスト2025 | Tatsuya Hasegawa | Webデザイナー",
+  description: "Tatsuya Hasegawa のポートフォリオサイト",
+};
 
 export default function PlayList2025() {
-  const isOpenHamburgerMenu = useAtomValue(isOpenHamburgerMenuAtom);
-
-  useSmoothScroll();
-
   return (
     <>
-      <div className={clsx(glassBox)}>
-        <Glass>
-          <header className={clsx(header)}>
-            <Nav />
-            <HamburgerMenu />
-            {isOpenHamburgerMenu && <MobileNav />}
-            <AudioPlayer />
-            <TextCircle />
-          </header>
-          <main className={clsx(main)}>
-            <WorksInfo
-              nameJa="プレイリスト2025"
-              nameEn="PLAYLIST2025"
-              nameJaNode={<>プレイリスト 2025</>}
-              nameEnNode={
-                <>
-                  PLAYLIST
-                  <br />
-                  2025
-                </>
-              }
-              imageSrc="/playlist2025.webp"
-              imageWidth={1000}
-              imageHeight={1000}
-              what="音楽サブスクリプション用プレイリストカバー"
-              why={
-                <>
-                  普段利用している音楽サブスクリプションのプレイリストカバーは自動で作成されるが、手を動かしてデザインの練習をする一環で、自分で手作りして設定したいと思ったことが制作のきっかけ。
-                </>
-              }
-              how={
-                <>
-                  日本流行色協会（JAFCA）が発表した2025年の色、
-                  空の青と海のエメラルドグリーンが溶け合う水平線の色、「ホライゾングリーン」を選択し、
-                  ポップなイラストを背景に敷くことでリラックス感を表現しました。
-                </>
-              }
-              role="Visual Design"
-              date="2025年1月"
-            />
-          </main>
-          <Footer />
-        </Glass>
-        <BackToIndex />
-      </div>
-      <Object1 />
-      <Object2 />
-      <Object3 />
+      <Works />;
     </>
   );
 }
