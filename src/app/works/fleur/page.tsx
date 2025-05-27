803d64c8009a5c1e7143d5ca250b97dd479323e8
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-05-27 13:24:48 +0900

    fix: works info

--- src/app/works/fleur/page.tsx
+++ src/app/works/fleur/page.tsx
@@ -2,7 +2,7 @@ import Works from "@/app/works/fleur/works";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "架空マカロン専門店フルール | Tatsuya Hasegawa | Webデザイナー",
+  title: "架空マカロン専門店『フルール』 | Tatsuya Hasegawa | Webデザイナー",
   description: "Tatsuya Hasegawa のポートフォリオサイト",
 };
 
