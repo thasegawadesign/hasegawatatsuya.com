9706fd0122a814bd3d1c2f86d700d714d7903663
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-08-02 18:36:47 +0900

    fix: metadata

--- src/app/works/playlist2025/page.tsx
+++ src/app/works/playlist2025/page.tsx
@@ -2,8 +2,7 @@ import Works from "@/app/works/playlist2025/works";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title: "プレイリスト2025 | Tatsuya Hasegawa | Webデザイナー",
-  description: "Tatsuya Hasegawa のポートフォリオサイト",
+  title: "プレイリスト2025 | 長谷川 達也のポートフォリオサイト",
 };
 
 export default function PlayList2025() {
