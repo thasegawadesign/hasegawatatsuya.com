b4fb4c934f65a5869e538c726f3bc31e57bb9780
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-09-03 01:20:40 +0900

    fix: update occupation

--- src/app/works/sporty/works.tsx
+++ src/app/works/sporty/works.tsx
@@ -37,12 +37,12 @@ export default function Works() {
           </header>
           <main className={clsx(main)}>
             <WorksInfo
-              nameJa="力強さのあるビジュアル"
+              nameJa="力強さのあるヴィジュアル"
               nameEn="Sporty"
               imageSrc="/sporty-mockup.avif"
               imageWidth={4000}
               imageHeight={3000}
-              what="スポーティで力強さのあるビジュアル"
+              what="スポーティで力強さのあるヴィジュアル"
               why={<>レタッチ技術の習得を兼ねて、作品として仕上げました。</>}
               how={
                 <>
