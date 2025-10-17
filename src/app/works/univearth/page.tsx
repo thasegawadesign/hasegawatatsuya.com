14a27d219c2139217bc142ae5cc55a7000afd068
thasegawadesign <hasegawatatsuya206@gmail.com>
2025-10-17 15:23:35 +0900

    refactor: metadata constants

--- src/app/works/univearth/page.tsx
+++ src/app/works/univearth/page.tsx
@@ -1,9 +1,9 @@
 import Works from "@/app/works/univearth/works";
+import { SITE_TITLE_BASE } from "@/constants/constants";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
-  title:
-    "「株式会社Univearth」コーポレートサイト | ポートフォリオ | Webデザイナー 長谷川 達也",
+  title: `「株式会社Univearth」コーポレートサイト | ${SITE_TITLE_BASE}`,
 };
 
 export default function Univearth() {
