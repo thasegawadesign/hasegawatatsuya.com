7e90a669a8d0130f15ae384e963c5ba6ca4ab523
thasegawadesign <hasegawatatsuya206@gmail.com>
2026-01-22 22:33:42 +0900

    chore: configure CSP headers

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,29 @@ const nextConfig = {
       },
     ];
   },
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: [
+              `default-src 'self'`,
+              `script-src 'self' ${process.env.NODE_ENV === "development" ? "'unsafe-eval'" : ""}`,
+              `style-src 'self' 'unsafe-inline' https://fonts.googleapis.com`,
+              `img-src 'self' data: https:`,
+              `font-src 'self' https://fonts.gstatic.com`,
+              `connect-src 'self' https://www.google-analytics.com https://vitals.vercel-insights.com`,
+              `frame-ancestors 'none'`,
+              `form-action 'self'`,
+              `base-uri 'self'`,
+            ].join("; "),
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default withVanillaExtract(nextConfig);
