3d9a7eca0df1c4cc4a2f063a9e5f34966789c698
thasegawadesign <hasegawatatsuya206@gmail.com>
2026-02-05 20:38:27 +0900

    feat: dynamic copyright year

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -2,12 +2,16 @@ import { roboto } from "@/app/fonts";
 import { copyRight, footer } from "@/components/footer/footer.css";
 import clsx from "clsx";
 
+export const revalidate = 86400;
+
 export default function Footer() {
+  const currentYear = new Date().getFullYear();
+
   return (
     <footer className={clsx(footer)}>
       <p>
         <small className={clsx(roboto.className, copyRight)}>
-          © {new Date().getFullYear()} Tatsuya Hasegawa
+          © {currentYear} Tatsuya Hasegawa
         </small>
       </p>
     </footer>
