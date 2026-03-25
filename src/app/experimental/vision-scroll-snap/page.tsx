400f88ba1a795a6c7a37a30932cd1385393f1d35
thasegawadesign <hello@hasegawatatsuya.com>
2026-03-25 23:58:38 +0900

    chore(experimental): mark experimental pages as experiments

--- src/app/experimental/vision-scroll-snap/page.tsx
+++ src/app/experimental/vision-scroll-snap/page.tsx
@@ -1,11 +1,11 @@
 import Works from "@/app/experimental/vision-scroll-snap/works";
-import { SITE_TITLE_WORKS } from "@/constants/constants";
+import { SITE_TITLE_EXPERIMENTAL } from "@/constants/constants";
 import { Metadata } from "next";
 
 export const dynamic = "force-static";
 
 export const metadata: Metadata = {
-  title: `Visionスクロールスナップ | ${SITE_TITLE_WORKS}`,
+  title: `Visionスクロールスナップ | ${SITE_TITLE_EXPERIMENTAL}`,
 };
 
 export default function VisionScrollSnapPage() {
