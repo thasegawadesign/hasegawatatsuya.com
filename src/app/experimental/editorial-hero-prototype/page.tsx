268dd164915d52bb0356b3e7b515d8b434dfc998
thasegawadesign <hello@hasegawatatsuya.com>
2026-03-26 00:09:05 +0900

    chore(experimental): update editorial hero prototype title

--- src/app/experimental/editorial-hero-prototype/page.tsx
+++ src/app/experimental/editorial-hero-prototype/page.tsx
@@ -5,7 +5,7 @@ import { Metadata } from "next";
 export const dynamic = "force-static";
 
 export const metadata: Metadata = {
-  title: `非対称ヒーロー プロトタイプ | ${SITE_TITLE_EXPERIMENTAL}`,
+  title: `大小2枚の非対称ヒーロー | ${SITE_TITLE_EXPERIMENTAL}`,
 };
 
 export default function EditorialHeroPage() {
